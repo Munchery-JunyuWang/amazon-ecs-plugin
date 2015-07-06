607500177ad7292ffd7f1b0e294c60d7583d5cb3
Reese Levine <reese.levine@munchery.com>
2015-07-06 09:54:33 -0700

    added logger for exception instead of printing it stdout

--- src/main/java/com/phuna/amazonecs/EcsCloud.java
+++ src/main/java/com/phuna/amazonecs/EcsCloud.java
@@ -132,70 +132,70 @@ public class EcsCloud extends Cloud implements AwsCloud {
 			final EcsTaskTemplate t = getTemplate(label);
 
 			logger.log(Level.INFO, "Will provision \"{0}\" for: {1}",
-					new Object[] { t.getTaskDefinitionArn(), label });
-
+				   new Object[] { t.getTaskDefinitionArn(), label });
+			
 			while (excessWorkload > 0) {
-				r.add(new NodeProvisioner.PlannedNode(t.getTaskDefinitionArn(),
-						Computer.threadPoolForRemoting
-								.submit(new Callable<Node>() {
-									public Node call() throws Exception {
-										EcsDockerSlave slave = null;
-										try {
-											slave = t
-													.provision(new StreamTaskListener(
+			    r.add(new NodeProvisioner.PlannedNode(t.getTaskDefinitionArn(),
+								  Computer.threadPoolForRemoting
+								  .submit(new Callable<Node>() {
+									  public Node call() throws Exception {
+									      EcsDockerSlave slave = null;
+									      try {
+										  slave = t
+										      .provision(new StreamTaskListener(
 															System.out));
-											final Jenkins jenkins = Jenkins
-													.getInstance();
-											// TODO once the baseline is 1.592+
-											// switch to Queue.withLock
-											synchronized (jenkins.getQueue()) {
-												jenkins.addNode(slave);
-											}
-											// Docker instances may have a long
-											// init script. If we declare
-											// the provisioning complete by
-											// returning without the connect
-											// operation, NodeProvisioner may
-											// decide that it still wants
-											// one more instance, because it
-											// sees that (1) all the slaves
-											// are offline (because it's still
-											// being launched) and
-											// (2) there's no capacity
-											// provisioned yet.
-											//
-											// deferring the completion of
-											// provisioning until the launch
-											// goes successful prevents this
-											// problem.
-											slave.toComputer().connect(false)
-													.get();
-											return slave;
-										} catch (Exception ex) {
-											logger.log(Level.SEVERE,
-													"Error in provisioning; slave="
-															+ slave
-															+ ", template=" + t);
-
-											ex.printStackTrace();
-											throw Throwables.propagate(ex);
-										} finally {
-											// TODO Decrease container counter??
-										}
-									}
-								}), t.getNumExecutors()));
-
-				excessWorkload -= t.getNumExecutors();
-
+										  final Jenkins jenkins = Jenkins
+										      .getInstance();
+										  // TODO once the baseline is 1.592+
+										  // switch to Queue.withLock
+										  synchronized (jenkins.getQueue()) {
+										      jenkins.addNode(slave);
+										  }
+										  // Docker instances may have a long
+										  // init script. If we declare
+										  // the provisioning complete by
+										  // returning without the connect
+										  // operation, NodeProvisioner may
+										  // decide that it still wants
+										  // one more instance, because it
+										  // sees that (1) all the slaves
+										  // are offline (because it's still
+										  // being launched) and
+										  // (2) there's no capacity
+										  // provisioned yet.
+										  //
+										  // deferring the completion of
+										  // provisioning until the launch
+										  // goes successful prevents this
+										  // problem.
+										  slave.toComputer().connect(false)
+										      .get();
+										  return slave;
+									      } catch (Exception ex) {
+										  logger.log(Level.SEVERE,
+											     "Error in provisioning; slave="
+											     + slave
+											     + ", template=" + t);
+										  logger.log(Level.SEVERE,
+											     ex.getMessage(), ex);
+										  throw Throwables.propagate(ex);
+									      } finally {
+										  // TODO Decrease container counter??
+									      }
+									  }
+								      }), t.getNumExecutors()));
+			    
+			    excessWorkload -= t.getNumExecutors();
+			    
 			}
 			return r;
 		} catch (Exception e) {
-			logger.log(Level.SEVERE, "Exception while provisioning for: "
-					+ label, e);
-			return Collections.emptyList();
+		    logger.log(Level.SEVERE, "Exception while provisioning for: "
+			       + label, e);
+		    return Collections.emptyList();
 		}
 	}
-
+    
 	@Override
 	public boolean canProvision(Label label) {
 
